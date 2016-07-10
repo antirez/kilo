c78b22f79a78154d7b91bebda43cb2071f9c3a69
Dagan Martinez <daganboy@gmail.com>
2016-07-10 18:18:01 -0400

    Added time.h(fGCC warning), added more C keywords

--- kilo.c
+++ kilo.c
@@ -40,6 +40,7 @@
 #include <termios.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
@@ -159,14 +160,15 @@ void editorSetStatusMessage(const char *fmt, ...);
  * There is no support to highlight patterns currently. */
 
 /* C / C++ */
-char *C_HL_extensions[] = {".c",".cpp",NULL};
+char *C_HL_extensions[] = {".c",".cpp",".h",NULL};
 char *C_HL_keywords[] = {
         /* A few C / C++ keywords */
         "switch","if","while","for","break","continue","return","else",
-        "struct","union","typedef","static","enum","class",
+        "struct","union","typedef","static","enum","class","volatile",
+	"extern","auto","const","do","default",
         /* C types */
         "int|","long|","double|","float|","char|","unsigned|","signed|",
-        "void|",NULL
+        "void|","short|",NULL
 };
 
 /* Here we define an array of syntax highlights by extensions, keywords,
