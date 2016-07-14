df08ef6de62d91382539da88d31a6c96e8187d70
aidara moussa <msaidara@gmail.com>
2016-07-14 13:00:25 +0200

    Separate declaration and and definition

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "kilo.h"
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr,"Usage: kilo <filename>\n");
+        exit(1);
+    }
+
+    initEditor();
+    editorSelectSyntaxHighlight(argv[1]);
+    editorOpen(argv[1]);
+    enableRawMode(STDIN_FILENO);
+    editorSetStatusMessage(
+        "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
+    while(1) {
+        editorRefreshScreen();
+        editorProcessKeypress(STDIN_FILENO);
+    }
+    return 0;
+}
