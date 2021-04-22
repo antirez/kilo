a3844aac1b6501c5a0c13b7099a868ba926ec888
rofl0r <rofl0r@users.noreply.github.com>
2021-04-22 16:13:35 +0100

    fix ub parsing multiline comments

--- kilo.c
+++ kilo.c
@@ -427,7 +427,7 @@ void editorUpdateSyntax(erow *row) {
         if (in_comment) {
             row->hl[i] = HL_MLCOMMENT;
             if (*p == mce[0] && *(p+1) == mce[1] && (!mce[2] || *(p+2) == mce[2])) {
-                for(++i,++p;mce[i];++i,++p)
+                for(++i,++p;i<row->rsize&&mce[i];++i,++p)
                     row->hl[i] = HL_MLCOMMENT;
                 in_comment = 0;
                 prev_sep = 1;
@@ -438,7 +438,7 @@ void editorUpdateSyntax(erow *row) {
                 continue;
             }
         } else if (*p == mcs[0] && *(p+1) == mcs[1] && (!mce[2] || *(p+2) == mce[2])) {
-            for(;mce[i];++i,++p)
+            for(;i<row->rsize&&mce[i];++i,++p)
                 row->hl[i] = HL_MLCOMMENT;
             in_comment = 1;
             prev_sep = 0;
