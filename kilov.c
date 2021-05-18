7208a509da86126eef3c340cc47938e9d6354730
Colk-tech <colk.tech@gmail.com>
2021-05-18 23:50:50 +0900

    [update] Improve users experience of UI

--- kilov.c
+++ kilov.c
@@ -58,6 +58,12 @@
 #define HL_NORMAL 0
 #define HL_NONPRINT 1
 
+/* Print text with color. */
+#define print_green(...) {printf("\x1b[32m"); printf(__VA_ARGS__); printf("\x1b[39m\n");}
+#define print_yellow(...) {printf("	\x1b[33m"); printf(__VA_ARGS__); printf("\x1b[39m\n");}
+#define print_error(...) {fprintf(stderr, "\x1b[31m"); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\x1b[39m\n");}
+#define fatal(exit_code, ...) {print_error(__VA_ARGS__); exit(exit_code);}
+
 /* This structure represents a single line of the file we are editing. */
 typedef struct erow {
     int idx;            /* Row index in the file, zero-based. */
@@ -689,8 +695,8 @@ void initEditor(void) {
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr,"Usage: kilo <filename>\n");
-        exit(1);
+        print_error("Error: Expected 2 arguments, but got %d.", argc);
+        fatal(1, "Usage: kilo <filename>");
     }
 
     initEditor();
